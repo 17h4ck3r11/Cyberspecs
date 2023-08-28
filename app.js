9ead7953ddfc417c59d6fbcc0b4f00917aded0ce
Cyberspecs-india <mvpkingpubg@gmail.com>
2023-08-28 21:03:07 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (dets) {
+  blur.style.left = dets.x + "px";
+  blur.style.top = dets.y + "px";
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      entry.isIntersecting
+          ? entry.target.classList.add("show")
+          : entry.target.classList.remove("show");
+  });
+});
+
+const hiddenElements = document.querySelectorAll("img.hidden");
+
+hiddenElements.forEach((el) => observer.observe(el));
