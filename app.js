515cd78b7d7016a3e9d07692480a982b662b9b48
Cyberspecs-india <mvpkingpubg@gmail.com>
2023-08-31 10:13:32 +0530

    Fixing a bug

--- app.js
+++ app.js
@@ -16,3 +16,5 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll("img.hidden");
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+
